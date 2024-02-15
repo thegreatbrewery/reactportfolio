433ef6c009affac9441c5fa5c4c723c0a40425ce
cedo037 <olungacedric@gmail.com>
2024-02-15 10:06:20 +0300

    Added netlify submit functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,16 +2,16 @@ export default function Contact() {
     return (
         <section className="contact" id="contact">
             <h2 className="heading">Contact <span>Me!</span></h2>
-            <form>
+            <form name="contact" method="POST" data-netlify="true">
                 <div className="input-box">
-                    <input type="text" placeholder="Full Name" />
-                    <input type="email" placeholder="Email Address" />
+                    <input type="text" placeholder="Full Name" name="fullName" />
+                    <input type="email" placeholder="Email Address" name="email"/>
                 </div>
                 <div className="input-box">
-                    <input type="number" placeholder="Mobile number" />
-                    <input type="text" placeholder="Email Subject" />
+                    <input type="number" placeholder="Mobile number" name="contactNumber" />
+                    <input type="text" placeholder="Email Subject" name="emailSubject"/>
                 </div>
-                <textarea name="" id="" cols="30" rows="10" placeholder="Your Message" />
+                <textarea name="meassage" cols="30" rows="10" placeholder="Your Message" />
                 <input type="submit" value="Send Message" class="btn" />
             </form>
         </section>
