49bb0815710b4a95a6ce6f9cadc8c9124b7feefa
cedo037 <olungacedric@gmail.com>
2024-02-16 11:10:35 +0300

    Changed contact form to submit on netlify

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,18 +2,19 @@ export default function Contact() {
     return (
         <section className="contact" id="contact">
             <h2 className="heading">Contact <span>Me!</span></h2>
-            <form name="Contact Form" method="POST" data-netlify="true">
-                <div className="input-box">
+            <form name="contact-form" method="POST" data-netlify="true">
+                <div class="input-box">
                     <input type="text" placeholder="Full Name" name="fullName" />
-                    <input type="email" placeholder="Email Address" name="email"/>
+                    <input type="email" placeholder="Email Address" name="email" />
                 </div>
-                <div className="input-box">
+                <div class="input-box">
                     <input type="number" placeholder="Mobile number" name="contactNumber" />
-                    <input type="text" placeholder="Email Subject" name="emailSubject"/>
+                    <input type="text" placeholder="Email Subject" name="emailSubject" />
                 </div>
-                <textarea name="meassage" cols="30" rows="10" placeholder="Your Message" />
+                <textarea name="message" cols="30" rows="10" placeholder="Your Message"></textarea>
                 <button type="submit" class="btn">Send message</button>
             </form>
+
         </section>
     );
 }
