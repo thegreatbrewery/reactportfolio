0e4c89134414b19f53a90eff67843f3eb76acd65
cedo037 <olungacedric@gmail.com>
2024-02-16 11:05:34 +0300

    Changed font

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact() {
     return (
         <section className="contact" id="contact">
             <h2 className="heading">Contact <span>Me!</span></h2>
-            <form name="contact" method="POST" data-netlify="true">
+            <form name="Contact Form" method="POST" data-netlify="true">
                 <div className="input-box">
                     <input type="text" placeholder="Full Name" name="fullName" />
                     <input type="email" placeholder="Email Address" name="email"/>
